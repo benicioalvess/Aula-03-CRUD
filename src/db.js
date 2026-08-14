d7610b9d6289c1f0c361e40924637c190f9a2b72
benicioalvess <beniciosantosalves@estudante.ifms.edu.br>
2026-08-14 16:13:12 -0400

    server upd2

--- src/db.js
+++ src/db.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "node:url"
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const DB_PATH = join(__dirname, "data.json");
+const PRODUCTS_PATH = join (__dirname, "products.json") 
 
 export async function  readUsers() {
     try {
@@ -19,4 +20,16 @@ export async function  readUsers() {
         
     }
     
+}
+
+export async function readProducts() {
+    try {
+        const raw = await readFile(PRODUCTS_PATH, "utf8");
+        return JSON.parse(raw)
+    } catch (error) {
+        if (error.code == 'ENOENT') return [];
+
+        throw error;
+
+    }
 }
--- src/server.js
+++ src/server.js
@@ -1,20 +1,25 @@
 import express from 'express'
-import { readUsers } from './db.js'
+import { readUsers, readProducts } from './db.js'
 
-const app = express ()
+const app = express()
 const PORT = 3000
 
 app.get('/users', async (req, res) => {
- const users = await readUsers ()
+    const users = await readUsers()
     res.json(users)
 })
 
 app.get("/users/:id", async (req, res) => {
     const users = await readUsers()
     const user = users.find(u => u.id === Number(req.params.id))
-     if (!user) return res.status(404).json({ erro: 'Usuário não encontrado' })
-    res.json(users)
+    if (!user) return res.status(404).json({ erro: 'Usuário não encontrado' })
+    res.json(user)
+})
+
+app.get('/products', async (req, res) =>{
+    const products = await readProducts()
+    res.json(products)
 })
 
 app.listen(PORT, () =>
-     console.log(`Server is running on http://localhost:${PORT}`))
+    console.log(`Server is running on http://localhost:${PORT}`))
